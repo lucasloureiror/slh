8cce24bffc8b67550ee88c28c40cfcbd1d9d2d4e
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2024-01-25 15:28:18 -0300

    fix: go fmt

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,15 +2,13 @@ package models
 
 type Config struct {
 	HoursPerDay int
-
 }
 
-
 type Downtime struct {
-	SLA string
-	Daily string
-	Weekly string
-	Monthly string
+	SLA      string
+	Daily    string
+	Weekly   string
+	Monthly  string
 	Quartely string
-	Yearly string
+	Yearly   string
 }
